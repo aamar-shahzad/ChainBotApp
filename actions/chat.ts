80d33ab1d669e8717bc0722271a4f421d80376cd
Shahzad <aamar.shahzad.1@ens.etsmtl.ca>
2024-03-16 16:50:04 -0400

    added some change

--- actions/chat.ts
+++ actions/chat.ts
@@ -135,7 +135,7 @@ const map = globalThis.ai_map ?? new Map<string, OpenAI>();
 async function createCompletion(message: string) {
   const payload = { inputs: message ,wait_for_model:true};
   const response = await fetch(
-    "https://api-inference.huggingface.co/models/google/gemma-7b",
+    `https://api-inference.huggingface.co/models/${process.env.ModelName}`,
     {
       headers: {
         Authorization: "Bearer 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",
@@ -147,5 +147,6 @@ async function createCompletion(message: string) {
   );
 
   const result = await response.json();
+  console.log(result)
   return result[0].generated_text;
 }
